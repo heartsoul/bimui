39ebdfcf59feaed3efc2309d33c2a006832d4531
soul <vc_zhjb@126.com>
2019-08-23 16:57:39 +0800

    [feat: 整理]  1. 提取公共文件

--- common/index.js
+++ common/index.js
@@ -1,6 +1,5 @@
 import { RNFetchBlob } from './extension';
 import { requestJSON, requestHTML } from './service/api+base';
-import NetUtil from './service/netutil';
 import callOnceInInterval from './utils/callOnceInInterval';
 import PinYinUtil from './utils/PinYinUtil';
 import * as mime from './utils/mime';
@@ -10,7 +9,6 @@ import * as config from './constant/server-config';
 
 export default {
     RNFetchBlob,
-    NetUtil,
     requestJSON,
     requestHTML,
     callOnceInInterval,
