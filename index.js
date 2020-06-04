11f1daf657d00cec71774f5c1a49213335629f66
张金波 <zhangjb-f@glodon.com>
2020-06-04 10:49:50 +0800

    [feat: 标准版]  1. 整理api，修改了到出， 将与业务相关的options处理删除，需要使用处单独处理。

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-import ThemeDefault from './AppTheme/AppTheme.default';
-import AppTheme from './AppTheme/AppTheme';
-import ART from './ART/ART';
-import common from './common';
+import { RNFetchBlob } from './extension';
+import { requestJSON, requestHTML, requestSimple } from './service/api+base';
+import callOnceInInterval from './utils/callOnceInInterval';
+import PinYinUtil from './utils/PinYinUtil';
+import * as mime from './utils/mime';
+import DateUtil from './utils/DateUtil';
+import * as ConsoleUtil from './utils/ConsoleUtil';
+import * as config from './constant/server-config';
 
-module.exports = {
-   ThemeDefault,
-   AppTheme,
-   ART,
-   ...common,
+export default {
+    RNFetchBlob,
+    fetch: {
+        requestJSON,
+        requestHTML,
+        requestSimple,
+    },
+    callOnceInInterval,
+    PinYinUtil,
+    mime,
+    DateUtil,
+    ConsoleUtil,
+    config,
 };
